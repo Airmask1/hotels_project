5ee9aa18f52f701432fae696c8b5a6a65cf0561f
ArmanEt <razzarioa@gmail.com>
2025-09-15 23:06:14 +0600

    Done with cookies

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,18 +1,33 @@
-from fastapi import APIRouter, HTTPException
-from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, Request, Response
 
 from src.database import async_session_maker
 from src.exceptions import UserAlreadyExists
 from src.repos.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd
+from src.services.auth import AuthService
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
+
+
+@router.post("/login")
+async def login_user(data: UserRequestAdd, response: Response):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(
+            email=data.email
+        )
+        if not user or not AuthService().verify_password(
+            plain_password=data.password, hashed_password=user.hashed_password
+        ):
+            raise HTTPException(status_code=401, detail="Incorrect user or password")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+
+    return {"access_token": access_token}
 
 
 @router.post("/register")
 async def register_user(data: UserRequestAdd):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         try:
@@ -21,3 +36,9 @@ async def register_user(data: UserRequestAdd):
             return {"status": "OK"}
         except UserAlreadyExists:
             raise HTTPException(status_code=500, detail="User already exists")
+
+
+@router.get("/only_auth")
+async def only_auth(request: Request):
+    access_token = request.cookies.get("access_token")
+    print(access_token)
--- src/repos/users.py
+++ src/repos/users.py
@@ -1,9 +1,9 @@
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 
 from src.exceptions import UserAlreadyExists
 from src.models.users import UsersOrm
 from src.repos.base import BaseRepository
-from src.schemas.users import User, UserAdd
+from src.schemas.users import User, UserAdd, UserWithHashedPassword
 
 
 class UsersRepository(BaseRepository):
@@ -16,3 +16,11 @@ class UsersRepository(BaseRepository):
             raise UserAlreadyExists("User with this email already exists")
         add_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         await self.session.scalars(add_stmt)
+
+    async def get_user_with_hashed_password(self, email: str):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+        if model:
+            return UserWithHashedPassword.model_validate(model, from_attributes=True)
+        return None
